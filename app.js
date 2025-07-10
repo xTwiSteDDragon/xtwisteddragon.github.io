15aa78a9c0f9eb19e4d7e4a42df89405d6eaefe1
xTwiSteDDragon <92927662+xTwiSteDDragon@users.noreply.github.com>
2025-07-10 16:08:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', './particlesjs-config.json', function() {
+  console.log('particles.js loaded - callback');
+});
